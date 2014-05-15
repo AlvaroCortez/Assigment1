ec84b954244ab15b3cdb9116d5e6fdc894851935
Maslakov Denis <maslakov_denis@mail.ru>
2014-05-15 19:33:12 +0400

    Change the names of Activity, added methods onStop, onStart

---
+++ SplashScreen/src/com/assigment1/splashscreen/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.assigment1.splashscreen;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class HomeActivity extends ListActivity {
+	final String[] catnames = new String[] { "Element1", "Element2",
+			"Element3", "Element4", "Element5", "Element6", "Element7",
+			"Element8", "Element9", "Element10", "Element11", "Element12",
+			"Element13", "Element14", "Element15", "Element16", "Element17",
+			"Element18", "Element19", "Element20", "" };
+	private ArrayAdapter<String> adapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ArrayList<String> list = new ArrayList<String>(Arrays.asList(catnames));
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, list);
+		setListAdapter(adapter);
+		setContentView(R.layout.activity_demo);
+	}
+}
